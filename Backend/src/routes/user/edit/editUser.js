3e9d38c5f034d431b57841c631f545228d0d656d
unknown <rodrigarlati@gmail.com>
2023-01-27 16:20:33 -0300

    advances

---
+++ Backend/src/routes/user/edit/editUser.js
@@ -0,0 +1,16 @@
+import { User } from "../../../database/models/User.js";
+
+export const editUser = async (req, res) => {
+    const {id, userName, image, password} = req.body
+    try {
+        const userUpdated = await User.update({
+            userName: userName,
+            image: image,
+            password: password
+        }, {where: {id: id}});
+
+        res.send(`User updated succesfully: ${userUpdated}`);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
--- Backend/src/routes/user/index.js
+++ Backend/src/routes/user/index.js
@@ -1,11 +1,14 @@
 import {Router} from "express";
 import { createUser } from "./register/userRegister.js";
 import { loginUser } from "./login/userLogin.js";
+import { editUser } from "./edit/editUser.js";
 
-const router = Router()
+const router = Router();
 
-router.post('/', createUser)
+router.post('/', createUser);
 
-router.get('/', loginUser)
+router.get('/', loginUser);
+
+router.put('/', editUser);
 
 export default router
