ee636a4e063f3b7794f41d08dba75ce6c389b321
unknown <rodrigarlati@gmail.com>
2023-03-27 19:30:29 -0300

    fix missing id

--- Frontend/src/pages/chats.tsx
+++ Frontend/src/pages/chats.tsx
@@ -1,10 +1,12 @@
-import React, { Fragment, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { apiUrl } from "@/utils/apiUrl"
 import { useAuth } from "@/context/AuthContext"
 import ChatCreator from "@/components/chats/ChatCreator"
 import ChatsManager from "@/components/chats/ChatsManager"
 
 export default function Chats(){
+    const [loading, setLoading] = useState(true)
+
     const {user, login, clearContext} = useAuth()
 
     useEffect(()=>{
@@ -13,16 +15,21 @@ export default function Chats(){
             fetch(`${apiUrl}/user/getuser/${id}`)
             .then(response => response.json())
             .then(data => login(data))
+            setLoading(false)
         }
     },[])
 
     return(
         <div>
-            {user.loggedUser? <h1 className="display-2 text-warning">Logged as  {user.loggedUser.userName}</h1> : null}
-            <ChatCreator />
-            <div className="bg-dark bg-opacity-50 rounded p-2 mt-2 chat-container">
-                <ChatsManager/>
-            </div>
+            {user.loggedUser? 
+            <div>
+                <h1 className="display-2 text-warning">Logged as  {user.loggedUser.userName}</h1>
+                <ChatCreator />
+                <div className="bg-dark bg-opacity-50 rounded p-2 mt-2 chat-container">
+                    <ChatsManager/>
+                </div>
+            </div>:
+            null}
         </div>
     )
 }
