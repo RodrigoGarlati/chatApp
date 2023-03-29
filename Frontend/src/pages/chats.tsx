65b538743e26b68665a63aa4f9e95bafe51f6b2f
unknown <rodrigarlati@gmail.com>
2023-03-29 18:41:00 -0300

    handle unlogged user

--- Frontend/src/pages/chats.tsx
+++ Frontend/src/pages/chats.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import { useRouter } from "next/router"
 import { apiUrl } from "@/utils/apiUrl"
 import { useAuth } from "@/context/AuthContext"
