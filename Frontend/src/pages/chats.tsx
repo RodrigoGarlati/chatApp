9c86d343785127a70488ba04a6775e874c9d4d42
unknown <rodrigarlati@gmail.com>
2023-03-29 18:39:52 -0300

    handle unlogged user

--- Frontend/src/pages/chats.tsx
+++ Frontend/src/pages/chats.tsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react"
+import { useRouter } from "next/router"
 import { apiUrl } from "@/utils/apiUrl"
 import { useAuth } from "@/context/AuthContext"
 import ChatCreator from "@/components/chats/ChatCreator"
 import ChatsManager from "@/components/chats/ChatsManager"
+import Logout from "@/components/logout/Logout"
 
 export default function Chats(){
-    const [loading, setLoading] = useState(true)
-
-    const {user, login, clearContext} = useAuth()
+    const {user, login} = useAuth()
+    const router = useRouter()
 
     useEffect(()=>{
         const id: string | null = localStorage.getItem('id')
-        if (!Object.keys(user).length){
+        if(!id){
+            router.push('/')
+        }
+        else if (!Object.keys(user).length){
             fetch(`${apiUrl}/user/getuser/${id}`)
             .then(response => response.json())
             .then(data => login(data))
-            setLoading(false)
         }
     },[])
 
@@ -23,8 +26,11 @@ export default function Chats(){
         <div>
             {user.loggedUser? 
             <div>
-                <h1 className="display-2 text-warning">Logged as  {user.loggedUser.userName}</h1>
-                <ChatCreator />
+                <h1 className="display-2 fw-normal text-light ">Logged as  {user.loggedUser.userName}</h1>
+                <div className="d-flex mt-4 ">
+                    <ChatCreator/>
+                    <Logout/>
+                </div>
                 <div className="bg-dark bg-opacity-50 rounded p-2 mt-2 chat-container">
                     <ChatsManager/>
                 </div>
